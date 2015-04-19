a14c81fbfd0466a752b0b1cf04eae9800959b9ae
FRodrigues42 <FRodrigues42@users.noreply.github.com>
2015-04-19 23:42:44 +0100

    Updated transport belts speed

--- inserters.js
+++ inserters.js
@@ -1,5 +1,5 @@
 inserters = {
-  "fast_transport_belt": {"id": "fast_transport_belt", "name": "Fast Transport Belt", "speed": 0.0625}, 
-  "basic_transport_belt": {"id": "basic_transport_belt", "name": "Transport Belt", "speed": 0.03125}, 
-  "express_transport_belt": {"id": "express_transport_belt", "name": "Express Transport Belt", "speed": 0.09375}
+  "fast_transport_belt": {"id": "fast_transport_belt", "name": "Fast Transport Belt", "speed": 0.19944}, 
+  "basic_transport_belt": {"id": "basic_transport_belt", "name": "Transport Belt", "speed": 0.32861}, 
+  "express_transport_belt": {"id": "express_transport_belt", "name": "Express Transport Belt", "speed": 0.49278}
 };
