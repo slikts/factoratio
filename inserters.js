ebcf06e6922ac8da1b1029c2afac166bdb677ac5
FRodrigues42 <FRodrigues42@users.noreply.github.com>
2015-04-20 01:32:56 +0100

    Changed order of the belts

--- inserters.js
+++ inserters.js
@@ -1,5 +1,5 @@
 inserters = {
+  "basic_transport_belt_inserter": {"id": "basic_transport_belt_inserter", "name": "Transport Belt", "speed": 0.19944},
   "fast_transport_belt_inserter": {"id": "fast_transport_belt_inserter", "name": "Fast Transport Belt", "speed": 0.32861}, 
-  "basic_transport_belt_inserter": {"id": "basic_transport_belt_inserter", "name": "Transport Belt", "speed": 0.19944}, 
   "express_transport_belt_inserter": {"id": "express_transport_belt_inserter", "name": "Express Transport Belt", "speed": 0.49278}
 };
